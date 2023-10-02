7aa692840b957453d97362fa343e992b1cb9ff89
Aakanksha Verma <92093809+aakankshaverma42@users.noreply.github.com>
2023-10-02 10:53:39 +0530

    first commit

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link, useNavigate } from 'react-router-dom';
+
+import {logo, sun } from '../assets';
+import { navlinks } from '../constants';
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
